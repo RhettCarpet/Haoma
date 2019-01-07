12f7d1863d711809f650517cbe537ac17a21beed
rhett buettrich <rhett.buettrich@hotmail.com>
2019-01-07 12:32:36 +0100

    Added the title to output

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,12 @@ url = "https://www.chefkoch.de/rs/s0/#{recipe}/Rezepte.html"
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
-html_doc.search('.search-list-item-title').each do |element|
-  puts element.text.strip
+html_doc.search('.search-list-item a').each do |element|
+  # puts element.css("search-list-item-content").css("search-list-item-title")
+  puts element.css(".search-list-item-content").css(".search-list-item-title").text
+  puts "https://www.chefkoch.de/" + element.attribute('href').value
 end
+
+
 
 
