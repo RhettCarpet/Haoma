cf3f1a7984baa57d3e8c257dead56106eeae5322
rhett buettrich <rhett.buettrich@hotmail.com>
2019-01-07 13:40:52 +0100

    Added the ingredients

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,11 +10,21 @@ url = "https://www.chefkoch.de/rs/s0/#{recipe}/Rezepte.html"
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
+
 html_doc.search('.search-list-item a').each do |element|
-  # puts element.css("search-list-item-content").css("search-list-item-title")
-  puts element.css(".search-list-item-content").css(".search-list-item-title").text
-  puts "https://www.chefkoch.de/" + element.attribute('href').value
+   puts element.css("search-list-item-content").css("search-list-item-title")
+   puts element.css(".search-list-item-content").css(".search-list-item-title").text
+   recipe_link = "https://www.chefkoch.de/" + element.attribute('href').value
+   puts recipe_link
+
+  recipe_html_file = open(recipe_link)
+  recipe_doc = Nokogiri::HTML(recipe_html_file)
+
+  recipe_doc.search('.incredients').each do |ingredient|
+    puts ingredient.css('td').text.gsub(/\s+/, "  ")
+  end
 end
+
 
 
 
